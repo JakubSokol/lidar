4a3f46259d8af707934fe2f63f3ca3876848b0bc
jakubsokol <kubas.sokol@gmail.com>
2024-05-23 21:35:52 +0200

    final fixes

--- main.py
+++ main.py
@@ -1,16 +1,56 @@
+from sensor_parser import Measurement
 from rplidar import RPLidar
-lidar = RPLidar(port='/dev/cu.usbserial-0001',timeout=3)
+import matplotlib.pyplot as plt
+import math
 
-info = lidar.get_info()
-print(info)
 
-health = lidar.get_health()
-print(health)
+PORT_NAME = '/dev/cu.usbserial-0001'
 
-for i, scan in enumerate(lidar.iter_scans()):
-    print('%d: Got %d measures' % (i, len(scan)))
-    if i > 10:
-        break
-lidar.stop()
-lidar.stop_motor()
-lidar.disconnect()
+def update_canvas(data, fig, line):
+    parsed_data=list()
+    for measurement in data:
+        if measurement.accuracy==0:
+            continue
+        else:
+            parsed_data.append(measurement)
+    a=list()
+    b=list()
+    for i in range(len(parsed_data)):
+        a.append(math.radians(parsed_data[i].angle))
+        b.append(parsed_data[i].distance)
+    print(a)
+    print(b)
+    line.set_data(a, b)
+    fig.canvas.draw()
+    fig.canvas.flush_events()
+
+                        
+
+def run():
+    '''Main function'''
+    lidar = RPLidar(PORT_NAME,timeout=3)
+    snapshot = list()
+    plt.ion()
+    fig, ax =plt.subplots(subplot_kw={'projection':'polar'})
+    line, =ax.plot([0],[1200], 'ro', markersize=2, markeredgecolor="r")
+    try:
+        print('Recording measurments... Press Crl+C to stop.')
+        counter = 1
+        for measurment in lidar.iter_measurments():
+            if measurment[0]==True:
+                counter -= 1
+                if counter == 0:
+                    counter = 2
+                    update_canvas(snapshot, fig, line)
+                snapshot=list()
+                snapshot.append(Measurement(accuracy=measurment[1], angle=measurment[2], distance=measurment[3]))
+            else:
+                if measurment[0]==False:
+                    snapshot.append(Measurement(accuracy=measurment[1], angle=measurment[2], distance=measurment[3]))
+    except KeyboardInterrupt:
+        print('Stopped.')
+    lidar.stop()
+    lidar.disconnect()
+
+if __name__ == '__main__':
+    run()
